2bbe64de1d8ee84f5d86890191a52940071e8de9
Petr Saganov <saganoff@gmail.com>
2019-07-17 22:45:04 +0300

    fix: Utilize Update/Delete on client

--- public/client/js/main.js
+++ public/client/js/main.js
@@ -13,7 +13,18 @@
                 document.getElementById('ranges').innerHTML = ranges.join("\n");
                 document.getElementById('create').onclick = function (e) {
                     add(e.target);
-                }
+                };
+                document.querySelectorAll('input[type=button][value=Update]').forEach(function(button) {
+                    button.onclick = function(e){
+                        add(e.target);
+                    }
+                });
+                document.querySelectorAll('input[type=button][value=Delete]').forEach(function(button) {
+                    button.onclick = function(e){
+                        del(e.target);
+                    }
+                });
+
             },
             'error': function () {
                 console.error('Ranges retrieving fail');
@@ -28,8 +39,8 @@
             `<input type="date" name="start" pattern="[0-9]{4}-[0-9]{2}-[0-9]{2}" value="${row.start}">`,
             `<input type="date" name="end" pattern="[0-9]{4}-[0-9]{2}-[0-9]{2}" value="${row.end}">`,
             `<input type="number" name="price" step="0.01" min="0" value="${row.price}">`,
-            '<input type="button" value="Update" id="update">',
-            '<input type="button" value="Delete" id="delete">',
+            '<input type="button" value="Update">',
+            '<input type="button" value="Delete">',
             '</div>'
         ].join("\n");
     };
@@ -59,7 +70,27 @@
                 init();
             },
             'error': function () {
-                console.error('Ranges retrieving fail');
+                console.error('Ranges updating fail');
+            }
+        };
+        apiCall(request);
+    };
+
+    let del = function(target) {
+        const body = {
+            'start': target.parentNode.querySelector("input[name=start]").value,
+            'end': target.parentNode.querySelector("input[name=end]").value,
+            'price': target.parentNode.querySelector("input[name=price]").value
+        };
+        const request = {
+            'method': 'DELETE',
+            'url': '/api/v1/ranges/',
+            'body': body,
+            'action': function () {
+                init();
+            },
+            'error': function () {
+                console.error('Ranges deleting fail');
             }
         };
         apiCall(request);
